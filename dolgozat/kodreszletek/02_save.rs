60f0c6a823a0805877cd9344f227a9797b382d48
Varga Dániel <varga.daniel@protonmail.com>
2019-04-23 20:01:28 +0200

    Mentés: fájlba kiírás metódusa.

--- dolgozat/kodreszletek/02_save.rs
+++ dolgozat/kodreszletek/02_save.rs
@@ -72,3 +72,96 @@ impl Drop for SaveDialog {
         self.0.destroy();
     }
 }
+
+// Save
+
+pub enum SaveAction {
+    New(ActiveMetadata),
+    Saved,
+    Canceled,
+}
+
+fn write_data(path: Option<&ActiveMetadata>, data: &[u8]) 
+    -> io::Result<SaveAction> 
+{
+    if let Some(path) = path {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(path.get_path())?;
+        file.write_all(&data)?;
+        return Ok(SaveAction::Saved);
+    }
+
+    let save_dialog = SaveDialog::new(None);
+    if let Some(new_path) = save_dialog.run() {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(false)
+            .open(&new_path)?;
+        file.write_all(data)?;
+        Ok(SaveAction::New(ActiveMetadata::new(new_path, data)))
+    } else {
+        Ok(SaveAction::Canceled)
+    }
+}
+
+pub fn save(
+    editor: &Buffer,
+    headerbar: &HeaderBar,
+    save: &Button,
+    current_file: &RwLock<Option<ActiveMetadata>>,
+    save_as: bool,
+) {
+    if let Some(text) = get_buffer(editor) {
+        let result = if save_as {
+            write_data(None, text.as_bytes())
+        } else {
+            write_data(current_file.read().unwrap().as_ref(), 
+                text.as_bytes())
+        };
+
+        match result {
+            Ok(SaveAction::New(file)) => {
+                set_title(&headerbar, file.get_path());
+                if let Some(parent) = file.get_dir() {
+                    let subtitle: &str = &parent.to_string_lossy();
+                    headerbar.set_subtitle(subtitle);
+                };
+
+                let mut current_file = current_file.write().unwrap();
+                *current_file = Some(file);
+                save.set_sensitive(false);
+            }
+            Ok(SaveAction::Saved) => {
+                if let Some(ref mut current_file) = 
+                    *current_file.write().unwrap() 
+                {
+                    current_file.set_sum(&text.as_bytes());
+                    save.set_sensitive(false);
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+// If let és árnyékolás magyarázat
+
+match path {
+    Some(letezik) => {
+        let kicsomagolt_letezik = letezik.unwrap();
+        // ...
+    },
+    _ => (),
+}
+
+let Some(letezik) = path {
+    // ...
+}
+
+let Some(path) = path {
+    // ...
+}
